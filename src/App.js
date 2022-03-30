2767c2e85edb576e5358d58c57e3f30023c04094
johc0205 <johc0205@naver.com>
2022-03-30 10:34:43 +0900

    until 2-3

--- src/App.js
+++ src/App.js
@@ -4,28 +4,15 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props)=>props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.span`
-  color: white;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required : true, minlength : 10})`
+  background-color: tomato;
 `;
 
 function App() {
   return( 
-    <Father>
-    <Box bgColor="teal">
-      <Text>Hello!</Text>
-    </Box>
-    <Circle bgColor="purple"/>
-  </Father>
+    <Father as="header">
+      <Input />
+    </Father>
   );
 }
 
